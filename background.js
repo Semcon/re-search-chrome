529085cc04d62bbd6ac240e86b53989618a70943
Sara Amani <sara.amani@semcon.com>
2016-10-10 14:32:54 +0200

    Added a button that user is supposed to use to turn on/off extension.

---
+++ background.js
@@ -0,0 +1,64 @@
+
+
+var val ="";
+//chrome.storage.sync.clear();
+
+//First time running script to check what value runState is in chrome storage.
+//If runState is undefined it is gets set to enabled otherwise it gets the value.
+chrome.storage.sync.get("runState", function(data) {
+  val = data.runState;
+  console.log('val: ', val);
+  if(typeof val === 'undefined'){
+    console.log('value is undefined');
+    chrome.storage.sync.set({'runState': 'enabled'}, function () {
+      console.log('Saved', 'runState', 'enabled');
+      val = 'enabled';
+      console.log('val: ', val);
+    });
+  }
+  return true;
+});
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    //From content script
+    if (request.runState === '?'){
+      chrome.storage.sync.get("runState", function(data) {
+        console.log('Value after getting it again: ', data.runState);
+        val = data.runState;
+        console.log('response: ', val);
+        sendResponse({runState: val});
+      });
+    }
+    //From popup
+    else if(request.runState === 'changeState'){
+      console.log('ChangeState from popup / current value is: ' , val);
+      if(val  === 'enabled'){
+        chrome.storage.sync.set({'runState': 'disabled'}, function () {
+          console.log('Saved', 'runState', 'disabled');
+          val = 'disabled';
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {runState: "disabled"}, function(response) {
+              console.log(response.message);
+            });
+          });
+        });
+      }
+      else if(val === 'disabled'){
+        chrome.storage.sync.set({'runState': 'enabled'}, function () {
+          console.log('Saved', 'runState', 'enabled');
+          val = 'enabled';
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {runState: "enabled"}, function(response) {
+                console.log(response.message);
+            });
+          });
+        });
+      }
+    }
+    else{
+      console.log('Message to event page was not handled');
+    }
+    return true;
+});
--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+var runState;
+
+
 function outputText( text ){
     console.log( text );
     var para = document.createElement("P");
@@ -23,7 +26,46 @@ function getSearchTerm(){
   });
 }
 
+
+//first time content script runs
 if( document.readyState === 'complete' ){
     console.log('document is complete');
-    getSearchTerm();
+
+    chrome.runtime.sendMessage({runState: "?"}, function(response) {
+      runState = response.runState;
+      console.log('runState in contentscript: ', runState);
+
+      if(runState === 'enabled'){
+        console.log('in runstate equals enabled');
+        getSearchTerm();
+      }
+      else if(runState === 'disabled'){
+        console.log('runState is not enabled');
+      }
+    });
 }
+
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.runState === 'disabled'){
+      runState = request.runState;
+      console.log('runstate: ', runState);
+      sendResponse({message: 'received disabled'});
+    }
+    else if(request.runState === 'enabled'){
+      runState = request.runState;
+      console.log('runstate: ', runState);
+
+      if( document.readyState === 'complete' ){
+          console.log('document is complete');
+          getSearchTerm();
+      }
+      sendResponse({message: 'received enabled'});
+    }
+    else{
+      console.log('Message from event page was not handled');
+    }
+    return true;
+});
