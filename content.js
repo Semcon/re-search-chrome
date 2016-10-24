c9fe358a9927d76a3ec1e0bf89fad70966e2028b
Sara Amani <sara.amani@semcon.com>
2016-10-24 16:44:06 +0200

    Error handling for drop-down list. Comments added for better understanding.

--- content.js
+++ content.js
@@ -30,51 +30,53 @@
             sendText( element.value );
         }
 
+        //Gets value from search field if enter is pressed
+        window.addEventListener( 'keydown', function( event ){
+            if( event.keyCode === 13 ){
+                console.log('enter was pressed');
+                sendText(element.value);
+            }
+        });
 
-          window.addEventListener( 'keydown', function( event ){
-              if( event.keyCode === 13 ){
-                  console.log('enter was pressed');
-                  sendText(element.value);
+        //Gets value autocomplete and checks parent and child elements
+        if(typeof selectorAutoComplete !== 'undefined'){
+          window.addEventListener('click', function (event) {
+              if( String(event.target.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
+                  console.log('autocomplete was clicked');
+                  sendText(event.target.outerText);
+              } else if(String(event.target.parentElement.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
+                  console.log('autocomplete was clicked');
+                  sendText(event.target.parentElement.outerText);
+              } else if(event.target.children.length > 0){
+                  for( var i = 0; i < event.target.children.length; i++ ){
+                      if(event.target.children[i].children.length > 0){
+                          for( var j = 0; j < event.target.children[i].children.length; j++ ){
+                              if(String(event.target.children[i].children[j].className).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1){
+                                  console.log('autocomplete was clicked');
+                                  sendText( event.target.children[i].children[j].outerText );
+                              }
+                          }
+                      }
+                  }
               }
           });
+        }
 
-          if(typeof selectorAutoComplete !== 'undefined'){
-            window.addEventListener('click', function (event) {
-                if( String(event.target.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
-                    console.log('autocomplete was clicked');
-                    sendText(event.target.outerText);
-                } else if(String(event.target.parentElement.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
-                    console.log('autocomplete was clicked');
-                    sendText(event.target.parentElement.outerText);
-                } else if(event.target.children.length > 0){
-                    for( var i = 0; i < event.target.children.length; i++ ){
-                        if(event.target.children[i].children.length > 0){
-                            for( var j = 0; j < event.target.children[i].children.length; j++ ){
-                                if(String(event.target.children[i].children[j].className).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1){
-                                    console.log('autocomplete was clicked');
-                                    sendText( event.target.children[i].children[j].outerText );
-                                }
-                            }
-                        }
-                    }
-                }
-            });
-          }
-
+        //Gets value from searchField when search button is clicked
         if(typeof selectorButton !== 'undefined'){
           document.querySelectorAll(selectorButton)[0].addEventListener('click', function () {
-              console.log('search button was clicked');
               sendText(element.value);
           });
         }
 
-        if(typeof document.getElementById('termList') !== 'undefined'){
-          document.getElementById('termList').addEventListener('change', function(event){
-            sendText(document.getElementById('termList').value);
-            document.querySelectorAll(selectorInput)[0].value = document.getElementById('termList').value;
-            document.getElementById("termList").selectedIndex = 0;
-          });
 
+        //Gets value from drop-down list
+        if(document.getElementById('termList') !== null){
+            document.getElementById('termList').addEventListener('change', function(event){
+                sendText(document.getElementById('termList').value);
+                document.querySelectorAll(selectorInput)[0].value = document.getElementById('termList').value;
+                document.getElementById("termList").selectedIndex = 0;
+            });
         }
     }
 
@@ -93,7 +95,6 @@
         defaultOption.value = 'Other Re-search terms';
         defaultOption.text = 'Other Re-search terms';
         selectList.add(defaultOption);
-
       }
 
       //Create and append the options
@@ -112,11 +113,11 @@
             url: window.location.href
         }, function(response) {
             if( response.selectorSearchField !== false ){
-                getSearchTerm(response.selectorSearchField, response.selectorButton, response.selectorAutoComplete);
                 if(runSetUI !== false){
                   setUI(response.selectorSearchField, response.terms);
                   runSetUI = false;
                 }
+                getSearchTerm(response.selectorSearchField, response.selectorButton, response.selectorAutoComplete);
             } else {
                 console.log('Selector not found');
             }
@@ -131,15 +132,12 @@
 
         console.log('document is complete');
 
-        console.log('Run init: ', runInit);
         chrome.runtime.sendMessage({
             runState: "?"
         }, function(response) {
             runState = response.runState;
-            console.log('runState in contentscript: ', runState);
 
             if(runState === 'enabled' && runInit === true){
-                console.log('runstate = enabled and runInit = true');
                 init();
                 runInit = false;
             } else if( runState === 'disabled' ){
@@ -156,15 +154,11 @@
             if ( request.runState === 'disabled' ){
                 runState = request.runState;
 
-                console.log( 'runstate: ', runState );
-
                 sendResponse({
                     message: 'received disabled'
                 });
             } else if( request.runState === 'enabled' ){
                 runState = request.runState;
-                console.log( 'runstate: ', runState );
-
                 if( document.readyState === 'complete' ){
                     console.log('document is complete');
                     if( runInit === true ){
