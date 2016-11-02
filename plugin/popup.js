a12537b7fa98a1a9cc4b6b6242782df2b2446035
kokarn <kokarn@gmail.com>
2016-11-02 15:29:02 +0100

    Add a disclaimer with the current version for easier debugging

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,4 +1,5 @@
 (function(){
+    var jsonData = false;
 
     function switchState( element ){
         if( element.classList.contains( 'switchbtn-on' ) ) {
@@ -10,8 +11,28 @@
         }
     }
 
+    function setVersion(){
+        if( !jsonData ){
+            setTimeout( setVersion, 64 );
+            return false;
+        }
+
+        document.querySelectorAll( '.js-disclaimer' )[ 0 ].innerText = jsonData.version;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open( 'GET', 'manifest.json', true );
+    xhr.onreadystatechange = function() {
+        if ( xhr.readyState === 4 && xhr.status === 200 ) {
+            jsonData = JSON.parse( xhr.responseText );
+        }
+    }
+    xhr.send();
+
     document.addEventListener('DOMContentLoaded', function () {
         console.log('domContentLoaded');
+
+        setVersion();
 
         document.querySelectorAll( '.js-power-button' )[ 0 ].addEventListener("click", function( event ){
             chrome.runtime.sendMessage({
